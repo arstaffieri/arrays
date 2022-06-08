9d567ee4032dfc84994bb26a2f258233c12cbf35
Angie Staffieri <angie.staffieri@gmail.com>
2022-06-08 11:03:56 -0600

    Do something to each array

--- arrays.js
+++ arrays.js
@@ -1,3 +1,9 @@
 var friends = ["Estelle", "Kate", "Nicki", "Greg"]
 var ages = [1, 2, 3, 4]
 var truths = [true, false, true, false]
+//This will reomove the last element in the array "friends" and return it. It should show "Greg"
+console.pop(friends)
+//This will remove the first item in the array "ages" and return it. It should show "1"
+console.shift(ages)
+//This will add an element to the end of the array and then tell me the new lenght of the array. What's it going to add? How does it know what to add?
+console.push(truths)
